b728c58e526dd7e94c2e4f7e4fba747086d0b5ef
Toan Le <toanle3322@gmail.com>
2024-03-13 19:44:08 +0700

    first commit Toan Le

---
+++ footer.php
@@ -0,0 +1,38 @@
+    <!-- -------------------------Footer -->
+    <section class="footer">
+        <div class="footer-container">
+            <p>Tải ứng dụng Tmen moda</p>
+            <div class="app-google">
+                <a href=""><img src="image/appstore.jpg" alt=""></a>
+                <a href=""> <img src="image/googleplay.jpg" alt=""></a>
+            </div>
+            <p>Nhận bản tin Tmen moda</p>
+            <div class="input-email">
+                <input type="text" placeholder="Nhập email của bạn">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+            <div class="footer-items">
+                <li><a href=""><img src="image/dathongbao.png" alt=""></a></li>
+                <li><a href="">Liên hệ</a></li>
+                <li><a href="">Tuyển dụng</a></li>
+                <li><a href="">Giới thiệu</a></li>
+                <li><a href=""><i class="fab fa-facebook-f"></i></a><a href=""><i class="fab fa-youtube"></i></a></li>
+            </div>
+            <div class="footer-text">
+                Công ty Cổ phần Tmen với số đăng ký kinh doanh: 0105777650 <br>
+Địa chỉ đăng ký: Số 674 Trường Sa Phường 17 Phú Nhuận,TP.HCM, Việt Nam - 0944809810<br>
+Đặt hàng online : 0944809811 .
+            </div>
+            <div class="footer-bottom">
+                ©Tmenmoda All rights reserved
+            </div>
+        </div>
+    </section>
+
+
+<script src="js/script.js"></script>
+<script src="js/slider.js"></script>
+
+
+</body>
+</html>
